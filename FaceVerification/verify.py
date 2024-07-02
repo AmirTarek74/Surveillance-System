f93258d7ece973b1fd6906cb06b40b8116bba3d8
Maryam <awadmaryam2@gmail.com>
2024-07-02 23:23:16 +0300

    Verification Function Updated

--- FaceVerification/verify.py
+++ FaceVerification/verify.py
@@ -5,20 +5,15 @@ class FaceVerification:
 
     def verify(self,emb1,emb2,metric='cosine'):
 
-        if metric=='euclidean':
-            threshold = 0.9
-            distance = find_euclidean_distance(emb1.cpu().numpy().reshape(-1,),emb2.cpu().numpy().reshape(-1,))
-            if distance < threshold:
-                return 1
-            else:
-                return 0
+        threshold = 0.9
+        distance = find_euclidean_distance(emb1.cpu().numpy().reshape(-1,),emb2.cpu().numpy().reshape(-1,))
+        if distance < threshold:
+            return 1
         
-        elif metric=='cosine':
-            threshold = 0.52
-            distance = find_cosine_distance(emb1.cpu().numpy().reshape(-1,),emb2.cpu().numpy().reshape(-1,))
-            if distance < threshold:
-                return 1
-            else:
-                return 0
+        threshold = 0.52
+        distance = find_cosine_distance(emb1.cpu().numpy().reshape(-1,),emb2.cpu().numpy().reshape(-1,))
+        if distance < threshold:
+            return 1
+        return 0
         
         
